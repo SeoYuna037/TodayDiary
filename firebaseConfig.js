c90fbc8b5c7ecb09829e8c50ecfd4f024a58da32
SeoYuna <tjdbsk037@gmail.com>
2022-03-27 17:33:28 +0900

    폴더 위치 변경

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCg8ul1Mqi0UIXY3nF6vwnkHrXkO-I5DPM",
+    authDomain: "firediay.firebaseapp.com",
+    databaseURL: "https://firediay-default-rtdb.firebaseio.com",
+    projectId: "firediay",
+    storageBucket: "firediay.appspot.com",
+    messagingSenderId: "565944170521",
+    appId: "1:565944170521:web:b258542056bf60158d1288"
+};
+firebase.initializeApp(firebaseConfig);
